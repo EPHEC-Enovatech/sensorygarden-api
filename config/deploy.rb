7d0ed10d28b521e84b3c86b2c421bbe08cce454e
Adrien Nini Pereira <adrien97nini@gmail.com>
2018-10-03 18:22:38 +0200

    Add deployment config

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,38 +2,28 @@
 lock "~> 3.11.0"
 
 set :application, 'sensorygarden-api'
-set :repo_url, 
+set :repo_url, 'git@github.com:EPHEC-Enovatech/sensorygarden-api.git'
+set :deploy_to, '/var/www/api.sensorygarden.be'
+set :user, 'deploy'
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets}
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+namespace :deploy do 
+    
+    %w[start stop restart].each do |command|
+        desc 'Manage Unicorn'
+        task command do
+            on roles(:app), in :sequence, wait: 1 do
+                execute "/etc/init.d/unicorn_#{fetch(:application)} #{command}"
+            end
+        end
+    end
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+    after :publishing, :restart
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+    after :restart, :clear_cache do
+        on roles(:web), in: :grous, limit: 3, wait: 10 do
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+        end
+    end
 
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+end
---
+++ config/unicorn.rb
@@ -0,0 +1,9 @@
+root = "/var/www/api.sensorygarden.be/current"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+listen "/tmp/unicorn.sensorygarden-api.sock"
+worker_processes 1
+timeout 30
