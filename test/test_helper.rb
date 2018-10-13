6e17dc796fcd45c4a5a4f13f4266976b9762cd22
Adrien Nini Pereira <adrien97nini@gmail.com>
2018-10-14 01:06:45 +0200

    Add coverage report

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,20 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+require 'simplecov'
+SimpleCov.start 'rails' do 
+  add_filter "/test/"
+end
+
+if ENV['CI'] == 'true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  
 end
