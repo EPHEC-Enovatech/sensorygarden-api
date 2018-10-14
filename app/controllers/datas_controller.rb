2dc8f96e2dc60cb9bebf60e02d28662e89d61185
Adrien Nini Pereira <adrien97nini@gmail.com>
2018-10-15 00:11:44 +0200

    fix cognitive complexity

--- app/controllers/datas_controller.rb
+++ app/controllers/datas_controller.rb
@@ -14,36 +14,28 @@ class DatasController < ApplicationController
   end
 
   def show_type
-    sensor_id = Sensor.find_by(sensorName: params[:data_type])
-    if sensor_id
-      records = DataRecord.find_by(device_id: params[:device_id], sensor_id: sensor_id)
-      if records
-        render json: { status: 'SUCCESS', message: 'Current records for requested device with this type', data: records }, status: :ok
-      else
-        render json: { status: 'ERROR', message: 'No record found for requested device with this type' }, status: :not_found
-      end
+    return unless sensor_id = get_sensor_id(params[:data_type])
+    records = DataRecord.find_by(device_id: params[:device_id], sensor_id: sensor_id)
+    if records
+      render json: { status: 'SUCCESS', message: 'Current records for requested device with this type', data: records }, status: :ok
     else
-      render json: { status: 'ERROR', message: 'This data type does not exist' }, status: :not_found
+      render json: { status: 'ERROR', message: 'No record found for requested device with this type' }, status: :not_found
     end
   end
 
   def create 
-    sensor_id = Sensor.find_by(sensorName: params[:data_type])
-    if sensor_id
-      record = DataRecord.new( device_id: params[:device_id], timestamp: params[:timestamp], sensor_id: sensor_id.id, data: params[:data] )
-      begin
-        record.save
-      rescue ActiveRecord::InvalidForeignKey => exception
-        render json: { status: 'ERROR', message: 'This device does not exist', data: exception.message }, status: :not_found
-      else
-        if record.id
-          render json: { status: 'SUCCESS', message: 'Record saved', data: record }, status: :ok
-        else
-          render json: { status: 'ERROR', message: 'Record save failed', data: record.errors }, status: :unprocessable_entity
-        end
-      end
+    return unless sensor_id = get_sensor_id(params[:data_type])
+    record = DataRecord.new( device_id: params[:device_id], timestamp: params[:timestamp], sensor_id: sensor_id.id, data: params[:data] )
+    begin
+      record.save
+    rescue ActiveRecord::InvalidForeignKey => exception
+      render json: { status: 'ERROR', message: 'This device does not exist', data: exception.message }, status: :not_found
     else
-      render json: { status: 'ERROR', message: 'This data type does not exist' }, status: :not_found
+      if record.id
+        render json: { status: 'SUCCESS', message: 'Record saved', data: record }, status: :ok
+      else
+        render json: { status: 'ERROR', message: 'Record save failed', data: record.errors }, status: :unprocessable_entity
+      end
     end
   end
 
@@ -54,6 +46,18 @@ class DatasController < ApplicationController
       render json: { status: 'SUCCESS', message: 'Record removed', data: record }, status: :ok
     else
       render json: { status: 'ERROR', message: 'Record not found' }, status: :not_found
+    end
+  end
+
+  private 
+
+  def get_sensor_id(sensor_name)
+    sensor_id = Sensor.find_by(sensorName: sensor_name)
+    if sensor_id
+      return sensor_id
+    else
+      render json: { status: 'ERROR', message: 'This data type does not exist' }, status: :not_found
+      return false
     end
   end
 
