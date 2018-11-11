bd4784f819bc117062b3ff801efcc1d0cd574025
Adrien Nini Pereira <adrien97nini@gmail.com>
2018-11-11 01:54:15 +0100

    Change production smtp to localhost

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.be',
+    address: 'localhost',
     port: 587,
     domain: 'sensorygarden.be',
     authentication: 'plain',
