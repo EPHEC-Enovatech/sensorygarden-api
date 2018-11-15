197224f3c024dbf4c86a0af9b3698a5db0221671
Adrien Nini Pereira <adrien97nini@gmail.com>
2018-11-15 15:39:20 +0100

    Change category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
+    has_many :posts, through: :categories_posts
+
     validates :categoryName, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,7 @@
 class Post < ApplicationRecord
-    has_one :categories, dependent: :destroy
+    has_many :categories, through: :categories_posts
     has_many :comments, through: :comments_posts
 
     validates :postTitle, presence: true
-    validates :postText, presence: true,
-    
+    validates :postText, presence: true
 end
