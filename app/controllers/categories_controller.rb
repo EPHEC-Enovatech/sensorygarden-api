e41c1dbf338f034af6f2971755a0ad2666684378
Adrien Nini Pereira <adrien97nini@gmail.com>
2018-12-13 14:29:02 +0100

    Fix authentication on categories endpoints

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
 
-    before_action :authenticate_user
+    before_action :authenticate_user, except: [:index, :show]
 
     def index
         render json: { status: 'SUCCESS', message: "All current catogries in database", data: Category.all }, status: :ok
